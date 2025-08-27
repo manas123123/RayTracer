abf8c54e72a796a1f6166aa3241f070de215f0b1
Manas Agrawal <agrawal.manas31@gmail.com>
2025-08-28 05:16:20 +0530

    Add Camera parameters and a working camera.

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -1,6 +1,7 @@
 package main;
 
-
+import camera.*;
+import utility.*;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -12,16 +13,29 @@ public class Driver {
     public static void main(String[] args) {
         long start = System.nanoTime();
 
-        Random random = new Random();
-        int width = 1920;
-        int height = 1080;
+        //Image Parameters
+        double aspectRatio = 16.0 / 9.0;
+        int height = 720;
+        int width = (int) (((double) (height)) * (aspectRatio));
+
+        //Camera Parameters
+        double viewport_height = 2.0;
+        double viewport_width = viewport_height * ((double) width / height);
+        double viewport_dis = 1.0;
+        Camera cam1 = new Camera(new Point3D(0, 0, 0), new Normal(0, 0, -1), viewport_height, viewport_width, 1.0);
+        cam1.viewport_x = new Vector3D(viewport_width, 0, 0);
+        cam1.viewport_y = new Vector3D(0, -viewport_height, 0);
+        cam1.writeDeltas(height, width);
+
+
+
 
         File image = new File("image.png");
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                bufferedImage.setRGB(x, y, 0);
+                bufferedImage.setRGB(x, y, cam1.getPixelAt(x, y, height, width).getRGB());
             }
         }
         try {
