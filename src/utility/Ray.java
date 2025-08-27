d767745ebb0bab7e3d601e7317f917d623470a4e
Manas Agrawal <agrawal.manas31@gmail.com>
2025-08-28 02:52:42 +0530

    Implement Ray Class.

---
+++ src/utility/Ray.java
@@ -0,0 +1,21 @@
+package utility;
+
+public class Ray {
+    public Point3D origin;
+    public Vector3D direction;
+
+    public Ray() {
+        origin = new Point3D();
+        direction = new Vector3D();
+    }
+
+    public Ray(Point3D origin, Vector3D direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+
+    public Ray(Ray ray) {
+        this.origin = ray.origin;
+        this.direction = ray.direction;
+    }
+}
