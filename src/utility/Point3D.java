f30f2b4f71c0074d8a74c5966eb4abdfc3216b3f
Manas Agrawal <agrawal.manas31@gmail.com>
2025-08-27 22:14:44 +0530

    Implement Point3D class.

---
+++ src/utility/Point3D.java
@@ -0,0 +1,28 @@
+package utility;
+
+public class Point3D {
+    public double x, y, z;
+
+    public Point3D() {
+        this(0, 0, 0);
+    }
+
+    public Point3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public Point3D(Point3D p) {
+
+        this(p.x, p.y, p.z);
+    }
+
+    public Point3D add(Point3D p) {
+        return new Point3D(x + p.x, y + p.y, z + p.z);
+    }
+
+    public Point3D sub(Point3D p) {
+        return new Point3D(x - p.x, y - p.y, z - p.z);
+    }
+}
