e194ad5fa34b89d954af32a034d10cfb754c48b8
Manas Agrawal <agrawal.manas31@gmail.com>
2025-08-29 01:00:38 +0530

    Implementing sphere class.

---
+++ src/Objects/Sphere.java
@@ -0,0 +1,15 @@
+package Objects;
+
+
+import utility.Point3D;
+
+
+public class Sphere {
+    public Point3D center;
+    public double radius;
+
+    public Sphere(Point3D center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+}
